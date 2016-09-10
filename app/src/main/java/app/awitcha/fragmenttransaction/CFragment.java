930f78863b4a2f0f322f50cd80cd0cc9c3f1bba9
songojame <song.cpe@gmail.com>
2016-09-10 16:27:19 +0700

    add fragment A B C and add animation to fragment A B C

---
+++ app/src/main/java/app/awitcha/fragmenttransaction/CFragment.java
@@ -0,0 +1,47 @@
+package app.awitcha.fragmenttransaction;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class CFragment extends Fragment {
+
+    private Button button;
+
+    public static CFragment newInstance() {
+        CFragment fragment = new CFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_c, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        button = (Button) view.findViewById(R.id.button_fragment_c);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager manager = getFragmentManager();
+                manager.popBackStack();
+            }
+        });
+    }
+}
